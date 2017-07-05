5988b43e41155d65393d8b7e1fe92b4835cef33c
Austin Westvik <austinwestvik@Austins-MacBook.local>
2017-07-04 15:08:04 -1000

    added .gitignore

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 #bot for reddit
 
 import praw
+import time
 
-praw.Reddit(
+import config
+
+def login():
+	praw.Reddit(
 		username = config.username,
 		password = config.password,
 		client_id = config.client_id,
 		client_secret = config.client_secret,
 		user_agent = config.user_agent)
+
+def run():
